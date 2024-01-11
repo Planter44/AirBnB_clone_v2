31f5a8709f057e2deab220f909bb1423583f8d52
Planter44 <planterplanter777@gmail.com>
2024-01-11 10:03:45 -0800

    Air2

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,38 +7,15 @@ from fabric.api import env, local, put, run, runs_once
 env.hosts = ["34.73.0.174", "35.196.78.105"]
 """The list of host server IP addresses."""
 
-@runs_once
-def do_pack():
-    """Files in Archives"""
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
-    cur_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        cur_time.year,
-        cur_time.month,
-        cur_time.day,
-        cur_time.hour,
-        cur_time.minute,
-        cur_time.second
-    )
-    try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        archize_size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
-    except Exception:
-        output = None
-    return output
-
 def do_deploy(archive_path):
     """Static files."""
-    if not os.path.exists(archive_path):
+    try os.path.exists(archive_path):
         return False
     file_name = os.path.basename(archive_path)
     folder_name = file_name.replace(".tgz", "")
     folder_path = "/data/web_static/releases/{}/".format(folder_name)
     success = False
-    try:
+else:
         put(archive_path, "/tmp/{}".format(file_name))
         run("mkdir -p {}".format(folder_path))
         run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
